8f6c91e117fee86f2b28b7b4add5e9aba55b003a
SIMI <simisann@gmail.com>
2023-01-08 03:51:33 +0400

    new get

--- src/main/java/com/jardemo/jarnew/JarnewApplication.java
+++ src/main/java/com/jardemo/jarnew/JarnewApplication.java
@@ -4,15 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 @SpringBootApplication
 public class JarnewApplication {
 
 
-	@GetMapping("/txt")
+	@RequestMapping(path = "/hello", method = RequestMethod.GET)
 	public String hello(){
 		return "hello";
 	}
